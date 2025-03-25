fe5f9464994b3b7ef2b8087a728e729b3284b000
Nanthakumar R <rnantha2002@gmail.com>
2025-03-25 19:42:13 +0530

    import and other fixes

--- ui.apps/src/main/content/jcr_root/apps/organik-aem-website/clientlibs/clientlib-site/js/common.js
+++ ui.apps/src/main/content/jcr_root/apps/organik-aem-website/clientlibs/clientlib-site/js/common.js
@@ -14,27 +14,7 @@ $(document).ready(function () {
     );
     hiddenElements.forEach((el) => observer.observe(el));
   }
-  // load more functionality
-  let contentItems = $(".organic-product-list");
-  const loadMoreButton = $("#load-more-button");
-  let initialCount = 4;
-  let loadedCount = initialCount;
-
-  // Show the initial set of cards
-  contentItems.slice(0, initialCount).show();
-
-  // Event listener for the Load More button
-  loadMoreButton.on("click", function () {
-    contentItems.slice(loadedCount, loadedCount + 4).show();
-    loadedCount += 4;
-    if (loadedCount >= contentItems.length) {
-      loadMoreButton.addClass("d-none");
-    }
-    // Hide the button if all items are shown
-    if (loadedCount >= contentItems.length) {
-      loadMoreButton.hide();
-    }
-  });
+  
   $(".explore-more").on("click", function () {
     $("html, body").animate(
       {
