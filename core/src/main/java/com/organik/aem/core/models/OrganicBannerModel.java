c8e59928d4a239e51b3ae8b9890a02bc417f3b4c
Nanthakumar R <rnantha2002@gmail.com>
2025-11-01 10:40:59 +0530

    latest changes

--- core/src/main/java/com/organik/aem/core/models/OrganicBannerModel.java
+++ core/src/main/java/com/organik/aem/core/models/OrganicBannerModel.java
@@ -8,11 +8,20 @@ import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import java.util.Collections;
 import java.util.List;
 
+import javax.inject.Inject;
+
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class OrganicBannerModel {
 
+    @Inject
+    private String bannerDescription;
+
     @ChildResource
     private List<OrganicBannerMultifield> bannerList;
+
+    public String getBannerDescription() {
+        return bannerDescription;
+    }
 
     public List<OrganicBannerMultifield> getBannerList() {
         return bannerList != null ? bannerList : Collections.emptyList();
